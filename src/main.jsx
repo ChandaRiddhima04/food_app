be8700d1b60d6870f34e6137cc23f7daf51ced3a
Riddhima Chanda <chandariddhima@gmail.com>
2025-07-31 12:32:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import IngredientScanner from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // ✅ Correct path with case and extension
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <IngredientScanner />
+    <App />
   </React.StrictMode>
 );
